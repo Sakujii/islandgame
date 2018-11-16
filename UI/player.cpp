6cf1be2858cf9d21145a570abcb5aa715ad1f6be
Jaakko Ekman <ekman3@linux-desktop8.cc.tut.fi>
2018-11-16 15:11:08 +0200

    Playerille lisätty metodit ja attribuutit

--- UI/player.cpp
+++ UI/player.cpp
@@ -2,5 +2,30 @@
 
 namespace Student{
 
+Player::Player(int id)
+{
+    _playerid=id;
+    _actionsLeft = 0;
+    _pawnLeft = 1;
+    _totalPawnCount = 1;
+}
+
+Player::~Player(){}
+
+int Player::getPlayerId()const
+{
+    return _playerid;
+}
+
+void Player::setActionsLeft(unsigned int actionsLeft)
+{
+    _actionsLeft = actionsLeft;
+}
+
+unsigned int Player::getActionsLeft() const
+{
+    return _actionsLeft;
+}
+
 
 }
--- UI/player.hh
+++ UI/player.hh
@@ -8,8 +8,36 @@ namespace Student{
 class Player : public Common::IPlayer
 {
 public:
-    Player();
+    Player(int id);
     ~Player();
+
+
+    /**
+     * @brief getPlayer gets the player id.
+     * @return The identifier of the player.
+     * @post Exception quarantee: nothrow
+     */
+    int getPlayerId() const;
+
+    /**
+     * @brief setActionsLeft sets the player's remaining number of actions.
+     * @param actionsLeft The number of actions left.
+     * @post Actions left set to parameter value. Exception quarantee: nothrow
+     */
+    void setActionsLeft(unsigned int actionsLeft);
+
+    /**
+     * @brief getActionsLeft returns the amount of actions left for the player
+     * @return amount of actions
+     * @post Exception quarantee: nothrow
+     */
+    unsigned int getActionsLeft() const;
+
+private:
+    int _playerid;
+    unsigned int _actionsLeft;
+    int _pawnLeft;
+    int _totalPawnCount;
 };
 }
 
