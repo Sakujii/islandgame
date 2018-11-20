54e9d15eee4244985d002ea47ee8cda273507b96
Saku Jussila <saku.jussila@student.tut.fi>
2018-11-20 12:34:35 +0100

    Pawn methods implemented in gameboard

--- UI/gameboard.cpp
+++ UI/gameboard.cpp
@@ -17,9 +17,11 @@ GameBoard::~GameBoard() = default;
 
 int GameBoard::checkTileOccupation(Common::CubeCoordinate tileCoord) const
 {
+    // If hex does not exist, return -1
     if (hexMap_.find(tileCoord) == hexMap_.end()){
         return -1;
     }
+    // Otherwise return Pawn amount
     else {
         auto it = hexMap_.find(tileCoord);
         std::shared_ptr<Common::Hex> hex = it->second;
@@ -47,11 +49,11 @@ bool GameBoard::isWaterTile(Common::CubeCoordinate tileCoord) const
 
 std::shared_ptr<Common::Hex> GameBoard::getHex(Common::CubeCoordinate hexCoord) const
 {
-    if (hexMap_.find(hexCoord) == hexMap_.end()){
+    auto it = hexMap_.find(hexCoord);
+    if (it == hexMap_.end()){
         return nullptr;
     }
     else{
-        auto it = hexMap_.find(hexCoord);
         std::shared_ptr<Common::Hex> hex = it->second;
         return hex;
     }
@@ -59,28 +61,65 @@ std::shared_ptr<Common::Hex> GameBoard::getHex(Common::CubeCoordinate hexCoord) 
 
 void GameBoard::addPawn(int playerId, int pawnId)
 {
-    std::cout << playerId << pawnId << std::endl;
+    Common::Pawn *newPawn = new Common::Pawn();
+    newPawn->setId(pawnId, playerId);
+    auto it = pawnMap_.find(pawnId);
+    if (it == pawnMap_.end()){
+        pawnMap_.insert(std::make_pair(pawnId, newPawn));
+    }
 }
 
 void GameBoard::addPawn(int playerId, int pawnId, Common::CubeCoordinate coord)
 {
-    std::cout << playerId << pawnId << coord.x << std::endl;
+    std::shared_ptr<Common::Pawn> newPawn = std::make_shared<Common::Pawn>();
+    newPawn->setId(pawnId, playerId);
+    newPawn->setCoordinates(coord);
+
+    auto it = pawnMap_.find(pawnId);
+    if (it == pawnMap_.end()){
+        std::shared_ptr<Common::Hex> hex = getHex(coord);
+        hex->addPawn(newPawn);
+        pawnMap_.insert(std::make_pair(pawnId, newPawn));
+    }
 }
 
 void GameBoard::movePawn(int pawnId, Common::CubeCoordinate pawnCoord)
 {
-    std::cout << pawnId << pawnCoord.x << std::endl;
+    // If Hex exists, move Pawn
+    if (hexMap_.find(pawnCoord) != hexMap_.end()){
+        auto it = pawnMap_.find(pawnId);
+        if (it != pawnMap_.end()){
+
+            // Remove Pawn from old Hex
+            std::shared_ptr<Common::Pawn> pawnPtr = it->second;
+            Common::CubeCoordinate oldCoord = pawnPtr->getCoordinates();
+            std::shared_ptr<Common::Hex> oldHex = getHex(oldCoord);
+            oldHex->removePawn(pawnPtr);
+
+            // Add pawn to new Hex and update Pawn coordinates
+            std::shared_ptr<Common::Hex> newHex = getHex(pawnCoord);
+            newHex->addPawn(pawnPtr);
+            pawnPtr->setCoordinates(pawnCoord);
+        }
+    }
 }
 
 void GameBoard::removePawn(int pawnId)
 {
-    std::cout << pawnId << std::endl;
+    auto it = pawnMap_.find(pawnId);
+    if (it != pawnMap_.end()){
+        std::shared_ptr<Common::Pawn> pawnPtr = it->second;
+        Common::CubeCoordinate oldCoord = pawnPtr->getCoordinates();
+        std::shared_ptr<Common::Hex> oldHex = getHex(oldCoord);
+        oldHex->removePawn(pawnPtr);
+
+        pawnMap_.erase(it);
+    }
 }
 
 void GameBoard::addActor(std::shared_ptr<Common::Actor> actor, Common::CubeCoordinate actorCoord)
 {
-    int id = actor->getId();
-    actorMap_.insert(std::make_pair(id, actorCoord));
+
 }
 
 void GameBoard::moveActor(int actorId, Common::CubeCoordinate actorCoord)
@@ -96,7 +135,6 @@ void GameBoard::removeActor(int actorId)
 void GameBoard::addHex(std::shared_ptr<Common::Hex> newHex)
 {
     Common::CubeCoordinate coord = newHex->getCoordinates();
-    // TODO: Koordinaattimuunnos ruudulle
     //Student::MainWindow* win = Student::MainWindow::getInstance();
     //win->drawHex(coord);
 
